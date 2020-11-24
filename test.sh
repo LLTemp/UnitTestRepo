5e5465ddb25b7f26fac2b2e7b0fc93d7a092070f
SergeyBrazhnik <sergey.brazhnik@ledgerleopard.com>
2020-11-24 20:26:49 +0300

    up

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-curl -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" https://api.github.com/repos/LLTemp/test_sic_zip/contents/new_one.txt
+curl -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/LLTemp/test_sic_zip/contents/new_one.txt
