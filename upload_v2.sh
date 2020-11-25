ccd36046115a6fa129b62489fdc4fd39a3f5dd52
SergeyBrazhnik <sergey.brazhnik@ledgerleopard.com>
2020-11-25 11:15:27 +0300

    up2

--- upload_v2.sh
+++ upload_v2.sh
@@ -1,3 +1,7 @@
-mkdir framework_holder
-cd framework_holder
+rm -rf test_sic_zip
 git clone https://github.com/LLTemp/test_sic_zip.git
+mv UnitTestWithPod.framework.zip test_sic_zip
+cd test_sic_zip
+git add .
+git commit -m "up"
+git push --repo=https://github.com/LLTemp/test_sic_zip.git
