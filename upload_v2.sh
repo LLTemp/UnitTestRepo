2ceb87fecb32498308107852f4db9b9d00f8dddf
SergeyBrazhnik <sergey.brazhnik@ledgerleopard.com>
2020-11-25 11:35:41 +0300

    up4

--- upload_v2.sh
+++ upload_v2.sh
@@ -4,4 +4,5 @@ mv UnitTestWithPod.framework.zip test_sic_zip
 cd test_sic_zip
 git add .
 git commit -m "up"
-git push https://LLTemp:2a9f49eac916d298408984ae7edaca903fd8df5d@github.com/LLTemp/test_sic_zip.git
+echo $GITHUB_TOKEN
+git push https://LLTemp:$GITHUB_TOKEN@github.com/LLTemp/test_sic_zip.git
