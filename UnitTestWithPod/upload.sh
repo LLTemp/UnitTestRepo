22a1c5d58383b19b6a3a9c990eecec265b96baca
SergeyBrazhnik <sergey.brazhnik@ledgerleopard.com>
2020-11-27 16:24:30 +0300

    up

--- UnitTestWithPod/upload.sh
+++ UnitTestWithPod/upload.sh
@@ -1,8 +1,10 @@
 rm -rf test_sic_zip
 git clone https://github.com/LLTemp/test_sic_zip.git
 mv UnitTestWithPod.framework.zip test_sic_zip
-cp SDK.podspec test_sic_zip
+cp UnitTestWithPod.podspec test_sic_zip
 cd test_sic_zip
+ls -la
 git add .
 git commit -m "up"
+git status
 git push https://LLTemp:$push_token@github.com/LLTemp/test_sic_zip.git
