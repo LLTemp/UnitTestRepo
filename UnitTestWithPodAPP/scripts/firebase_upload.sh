38641d7f4846ad626f0a6097caf7ad5bf1a0e914
SergeyBrazhnik <sergey.brazhnik@ledgerleopard.com>
2020-12-07 14:00:19 +0300

    up345678

--- UnitTestWithPodAPP/scripts/firebase_upload.sh
+++ UnitTestWithPodAPP/scripts/firebase_upload.sh
@@ -6,9 +6,10 @@ date=$(TZ=":Europe/Amsterdam" date +"%d-%m-%Y %H:%M:%S")
 release_notes="Log: $commit_message\nBuild date: $date"
 echo "relase_notes = $release_notes"
 
-firebase appdistribution:distribute  ./build/Cleverbase.ipa  \
+firebase appdistribution:distribute  ./build/UnitTestWithPodAPP.ipa  \
   --app "$FIREBASE_APP_ID"  \
   --release-notes "$release_notes" \
+  --groups "$GROUPS_LIST" \
   --token "$FIREBASE_TOKEN"
 
 
