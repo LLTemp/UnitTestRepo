5bb07e8274bc06e4a7462e58347add6a0f69bb07
sergeybrazhnik <6epreu@gmail.com>
2021-03-01 18:55:30 +0300

    commint new version of library with Swift Package Manager

---
+++ UnitTestWithPodSpm/UnitTestWithPodSpm/SDK.swift
@@ -0,0 +1,55 @@
+//
+//  SDK.swift
+//  UnitTestWithPodSpm
+//
+//  Created by Sergey Brazhnik on 01.03.2021.
+//
+
+import Foundation
+import KeychainSwift
+import UnitTestWithPodSpm.OpenSSL
+import Combine
+
+public class HelloLib: NSObject{
+
+    public static var shared : HelloLib = HelloLib()
+
+    let keychain = KeychainSwift(keyPrefix: "HelloLib")
+    
+    public func helloWorld(){
+        print("hello world")
+    }
+
+    public func returnZero() -> Int{
+        0
+    }
+    
+    public func saveString(key: String, value: String) {
+        keychain.set(value, forKey: key,withAccess: .accessibleWhenPasscodeSetThisDeviceOnly)
+    }
+
+    public func getString(key: String) -> String? {
+        keychain.get(key)
+    }
+    
+    public func md5() -> String{
+        var result = [UInt8](repeating: 0, count: Int(MD5_DIGEST_LENGTH))
+        let source = Array("hello".utf8)
+        MD5(source, source.count, &result)
+
+        let resData = Data(result)
+        return resData.hex
+    }
+}
+
+extension Data {
+    public var hex : String{
+            get {
+                let str = map { (element) -> String in
+                    String(format: "%02x", element)
+                }
+                return str.joined()
+            }
+        }
+
+}
---
+++ UnitTestWithPodSpm/UnitTestWithPodSpm/UnitTestWithPodSpm.h
@@ -0,0 +1,18 @@
+//
+//  UnitTestWithPodSpm.h
+//  UnitTestWithPodSpm
+//
+//  Created by Sergey Brazhnik on 01.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnitTestWithPodSpm.
+FOUNDATION_EXPORT double UnitTestWithPodSpmVersionNumber;
+
+//! Project version string for UnitTestWithPodSpm.
+FOUNDATION_EXPORT const unsigned char UnitTestWithPodSpmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitTestWithPodSpm/PublicHeader.h>
+
+
