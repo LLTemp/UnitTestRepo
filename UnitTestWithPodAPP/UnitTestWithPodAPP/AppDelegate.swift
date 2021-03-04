fe829d72db2a53789ba473539aba74de482e9897
sergeybrazhnik <6epreu@gmail.com>
2021-03-04 15:57:16 +0300

    asdfasdf

--- UnitTestWithPodAPP/UnitTestWithPodAPP/AppDelegate.swift
+++ UnitTestWithPodAPP/UnitTestWithPodAPP/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import UnitTestWithPod
+import UnitTestWithPodSpm
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- UnitTestWithPodAPP/scripts/sign_and_upload.sh
+++ UnitTestWithPodAPP/scripts/sign_and_upload.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 #################### ADD default keychain with certificates ####################
+echo "Delete a custom keychain if exists"
+security delete-keychain -p ci ios-build.keychain
+
 echo "Create a custom keychain"
 security create-keychain -p ci ios-build.keychain
 
@@ -17,7 +20,7 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 echo "Add certificates to keychain and allow codesign to access them"
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -A /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -A /usr/bin/codesign
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_SECRET -A /usr/bin/codesign
+security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P 1234567890 -A /usr/bin/codesign
 # Some MAGIC (TODO INVESTIGATE) which helps to NOT hangs the codesign
 security set-key-partition-list -S apple-tool:,apple: -s -k ci ios-build.keychain
 
@@ -29,24 +32,24 @@ cp "./scripts/profile/UnitTestRepoAppAdHocCI.mobileprovision" ~/Library/MobileDe
 
 #################### BUILD ARCHIVE ####################
 echo "Archive"
-xcodebuild -workspace ./UnitTestWithPodAPP.xcworkspace -scheme UnitTestWithPodAPP -sdk iphoneos -configuration Release archive -archivePath ./build/UnitTestWithPodAPP.xcarchive
+xcodebuild -project ./UnitTestWithPodAPP.xcodeproj -scheme UnitTestWithPodAPP -sdk iphoneos -configuration Release archive -archivePath ./build/UnitTestWithPodAPP.xcarchive
 echo "Create IPA and sign"
 xcodebuild -exportArchive -archivePath ./build/UnitTestWithPodAPP.xcarchive -exportOptionsPlist ./scripts/exportOptions.plist -exportPath ./build
 
-
-
-#################### Uploading application to Firebase ####################
-echo "Install firebase CLI"
-npm install -g firebase-tools
-
-echo "Upload to Firebase"
-commit_message=$(git log -1 --pretty=%B)
-date=$(TZ=":Europe/Amsterdam" date +"%d-%m-%Y %H:%M:%S")
-release_notes="Log: $commit_message\nBuild date: $date"
-echo "relase_notes = $release_notes"
-
-firebase appdistribution:distribute  ./build/UnitTestWithPodAPP.ipa  \
-  --app "$FIREBASE_APP_ID"  \
-  --release-notes "$release_notes" \
-  --groups "$GROUPS_LIST" \
-  --token "$FIREBASE_TOKEN"
+#
+#
+##################### Uploading application to Firebase ####################
+#echo "Install firebase CLI"
+#npm install -g firebase-tools
+#
+#echo "Upload to Firebase"
+#commit_message=$(git log -1 --pretty=%B)
+#date=$(TZ=":Europe/Amsterdam" date +"%d-%m-%Y %H:%M:%S")
+#release_notes="Log: $commit_message\nBuild date: $date"
+#echo "relase_notes = $release_notes"
+#
+#firebase appdistribution:distribute  ./build/UnitTestWithPodAPP.ipa  \
+#  --app "$FIREBASE_APP_ID"  \
+#  --release-notes "$release_notes" \
+#  --groups "$GROUPS_LIST" \
+#  --token "$FIREBASE_TOKEN"
